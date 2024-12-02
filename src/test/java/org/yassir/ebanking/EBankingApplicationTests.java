fd8fdd3dc23e992e8127baa039060f5bce6d56e8
yassir5236 <yassiraitlahmadi@gmail.com>
2024-12-02 10:25:22 +0100

    adding dependecies

---
+++ src/test/java/org/yassir/ebanking/EBankingApplicationTests.java
@@ -0,0 +1,13 @@
+package org.yassir.ebanking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EBankingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
